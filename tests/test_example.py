054748363fa76a4faf765ae025aede3dd35a6294
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2021-06-25 09:09:16 -0400

    Add assertion to verify that the root logger's logging level is set correctly
    See the discussion here for more context:
    https://github.com/cisagov/pe-reports/pull/6#discussion_r657329612

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env pytest -vs
 """Tests for example."""
 
 # Standard Python Libraries
@@ -88,6 +87,10 @@ def test_log_levels(level):
             assert (
                 logging.root.hasHandlers() is True
             ), "root logger should now have a handler"
+            assert (
+                logging.getLevelName(logging.root.getEffectiveLevel()) == level.upper()
+            ), f"root logger level should be set to {level.upper()}"
+            assert return_code is None, "main() should return success"
 
 
 def test_bad_log_level():
