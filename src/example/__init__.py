bcfbd51b2f76a389e09f6b81d1bb56978d36ab45
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2024-03-06 15:20:58 -0500

    Add whitespace to placate Black linter/formatter

--- src/example/__init__.py
+++ src/example/__init__.py
@@ -1,4 +1,5 @@
 """The example library."""
+
 # We disable a Flake8 check for "Module imported but unused (F401)" here because
 # although this import is not directly used, it populates the value
 # package_name.__version__, which is used to get version information about this
